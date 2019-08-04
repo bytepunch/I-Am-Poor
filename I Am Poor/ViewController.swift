33e5d7dd6100d520f1fb624f07deab435443808f
Jörg <bytepunch@gmail.com>
2019-08-04 11:58:08 +0200

    Initial commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Jörg Klausewitz on 04.08.19.
+//  Copyright © 2019 Jörg Klausewitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
